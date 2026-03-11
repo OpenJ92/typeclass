e02a26d1fb92c8a8b369d6bfb76f34068cbc2614
OpenJ92 <jacob.vartuli.92@gmail.com>
2026-03-10 23:00:13 -0400

    Clean up typeclass impls. Prep for lib.py

--- src/typeclass/data/sequence.py
+++ src/typeclass/data/sequence.py
@@ -6,29 +6,30 @@ from typeclass.protocols.applicative import Applicative
 from typeclass.protocols.alternative import Alternative
 from typeclass.protocols.monad import Monad
 from typeclass.protocols.show import Show
-from typeclass.protocols.eq import Eq
+from typeclass.protocols.force import Force
 
 A = TypeVar("A")
 B = TypeVar("B")
 
+class Sequence(Monad[A], Alternative, Applicative[A], Functor[A], Show, Generic[A]):
 
-class Seq(Monad, Alternative, Applicative[A], Functor[A], Show, Eq, Generic[A]):
     # ----- Functor ---------------------------------------------------------
 
-    def fmap(self: Seq[A], f: Callable[[A], B]) -> Seq[B]:
+    def fmap(self: Sequence[A], f: Force[Callable[[A], B]]) -> Sequence[B]:
+        _f = f.force()
         match self:
             case Cons(head=h, tail=t):
-                return Cons(f.force()(h), t.fmap(f))
+                return Cons(_f(h), t.fmap(f))
             case Nil():
                 return Nil()
 
     # ----- Applicative -----------------------------------------------------
 
     @classmethod
-    def pure(cls: type, value: A) -> Seq[A]:
+    def pure(cls: type, value: A) -> Sequence[A]:
         return Cons(value, Nil())
 
-    def ap(self: Seq[Callable[[A], B]], fa: Seq[A]) -> Seq[B]:
+    def ap(self: Sequence[Callable[[A], B]], fa: Force[Sequence[A]]) -> Sequence[B]:
         """
         Cartesian product applicative:
           [f1,f2] <*> [a,b] = [f1(a), f1(b), f2(a), f2(b)]
@@ -36,65 +37,41 @@ class Seq(Monad, Alternative, Applicative[A], Functor[A], Show, Eq, Generic[A]):
         fs = self
         xs = fa.force()
 
-        def map_all(f: Callable[[A], B], xs_: Seq[A]) -> Seq[B]:
-            match xs_:
-                case Cons(head=h, tail=t):
-                    return Cons(f(h), map_all(f, t))
-                case Nil():
-                    return Nil()
-
-        def concat(a: Seq[B], b: Seq[B]) -> Seq[B]:
-            match a:
-                case Cons(head=h, tail=t):
-                    return Cons(h, concat(t, b))
-                case Nil():
-                    return b
-
         match fs:
             case Cons(head=f, tail=rest):
-                return concat(map_all(f, xs), rest.ap(xs))
+                return concat(xs.fmap(Thunk(lambda: f)), rest.ap(fa))
             case Nil():
                 return Nil()
 
     # ----- Alternative -----------------------------------------------------
 
     @classmethod
-    def empty(cls: type) -> Seq[A]:
+    def empty(cls: type) -> Sequence[A]:
         return Nil()
 
-    def otherwise(self: Seq[A], other: Seq[A]) -> Seq[A]:
-        """
-        Choice for Seq = concatenation.
-        """
-        other_ = other.force()
-        match self:
-            case Cons(head=h, tail=t):
-                return Cons(h, t.otherwise(other_))
-            case Nil():
-                return other_
+    def otherwise(self: Sequence[A], other: Force[Sequence[A]]) -> Sequence[A]:
+        return concat(self, other.force())
 
     # ----- Monad -----------------------------------------------------------
 
-    def bind(self: Seq[A], mf: Callable[[A], Seq[B]]) -> Seq[B]:
+    def bind(self: Sequence[A], mf: Force[Callable[[A], Sequence[B]]]) -> Sequence[B]:
         mf_ = mf.force()
-
-        def concat(a: Seq[B], b: Seq[B]) -> Seq[B]:
-            match a:
-                case Cons(head=h, tail=t):
-                    return Cons(h, concat(t, b))
-                case Nil():
-                    return b
 
         match self:
             case Cons(head=h, tail=t):
-                return concat(mf_(h), t.bind(mf_))
+                return concat(mf_(h), t.bind(mf))
             case Nil():
                 return Nil()
+
+    # ----- Show ------------------------------------------------------------
+
+    def __repr__(self) -> str:
+        return f"Sequence({self.to_list()!r})"
 
     # ----- Convenience -----------------------------------------------------
 
     def __iter__(self) -> Iterator[A]:
-        cur: Seq[A] = self
+        cur: Sequence[A] = self
         while True:
             match cur:
                 case Cons(head=h, tail=t):
@@ -103,32 +80,19 @@ class Seq(Monad, Alternative, Applicative[A], Functor[A], Show, Eq, Generic[A]):
                 case Nil():
                     return
 
-    def to_list(self) -> list[A]:
-        return list(iter(self))
 
-    @staticmethod
-    def from_iterable(xs: Iterable[A]) -> Seq[A]:
-        out: Seq[A] = Nil()
-        # build from right to left
-        tmp = list(xs)
-        for x in reversed(tmp):
-            out = Cons(x, out)
-        return out
-
-    def __repr__(self) -> str:
-        return f"Seq({self.to_list()!r})"
-
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, Seq):
-            return False
-        return self.to_list() == other.to_list()
-
-
-class Cons(Seq[A]):
-    def __init__(self, head: A, tail: Seq[A]):
+class Cons(Sequence[A]):
+    def __init__(self, head: A, tail: Sequence[A]):
         self.head = head
         self.tail = tail
 
 
-class Nil(Seq[A]):
+class Nil(Sequence[A]):
     pass
+
+def concat(xs: Sequence[A], ys: Sequence[A]) -> Sequence[A]:
+    match xs:
+        case Cons(head=head, tail=tail):
+            return Cons(head, concat(tail,ys))
+        case Nil():
+            return ys
