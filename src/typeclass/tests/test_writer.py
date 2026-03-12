bf2df6c95639f623e0d842d9c1de9f1f0d020731
OpenJ92 <jacob.vartuli.92@gmail.com>
2026-03-12 12:28:54 -0400

    Writer test integration

---
+++ src/typeclass/tests/test_writer.py
@@ -0,0 +1,53 @@
+import unittest
+
+from typeclass.interpret.interpreter import interpret
+from typeclass.tests.fixtures import writer as fx_writer
+from typeclass.tests.laws.functor import (
+    functor_identity_expr,
+    functor_composition_expr,
+    functor_replace_expr,
+    functor_void_expr,
+)
+
+
+class WriterTestCase(unittest.TestCase):
+    def assert_expr_equal(self, lhs, rhs):
+        self.assertEqual(
+            interpret(lhs, None, None).force(),
+            interpret(rhs, None, None).force(),
+        )
+
+
+class TestWriterFunctor(WriterTestCase):
+    def test_functor_identity(self):
+        for value in fx_writer.values():
+            with self.subTest(value=value):
+                lhs, rhs = functor_identity_expr(value)
+                self.assert_expr_equal(lhs, rhs)
+
+    def test_functor_composition(self):
+        f = lambda x: x + 1
+        g = lambda x: x * x
+
+        for value in fx_writer.values():
+            with self.subTest(value=value):
+                lhs, rhs = functor_composition_expr(value, f, g)
+                self.assert_expr_equal(lhs, rhs)
+
+    def test_functor_replace(self):
+        replacement = fx_writer.replacement()
+
+        for value in fx_writer.values():
+            with self.subTest(value=value):
+                lhs, rhs = functor_replace_expr(value, replacement)
+                self.assert_expr_equal(lhs, rhs)
+
+    def test_functor_void(self):
+        for value in fx_writer.values():
+            with self.subTest(value=value):
+                lhs, rhs = functor_void_expr(value)
+                self.assert_expr_equal(lhs, rhs)
+
+
+if __name__ == "__main__":
+    unittest.main()
